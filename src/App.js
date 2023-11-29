dd15f53355d626b899c914cab91bb9e9c434300f
unknown <majid.asadollahi95@gmail.com>
2023-11-29 20:07:48 +0330

    add custom hook

--- src/App.js
+++ src/App.js
@@ -3,17 +3,36 @@ import React, { useContext } from 'react'
 import Products from './components/Products/Products'
 import Auth from './components/Auth'
 import { AuthContext } from './context/auth-context'
+import useDarkMode from './hooks/dark-mode'
 
 import './App.css'
 
 const App = (props) => {
+  const [theme, toggleTheme] = useDarkMode()
+
 
   const authContext = useContext(AuthContext)
 
   let content = <Auth />
 
   if (authContext.isAuth) {
-    content = <Products />
+    content = (
+      <div
+        className='app'
+        style={{
+          background: theme === 'dark' ? '#212121': '#ffffff',
+          color: theme === 'dark' ? '#ffffff': '#212121',
+          transition: '.2s all',
+          textAlign: 'center'
+        }}
+      >
+        <Products />
+
+        <button type='button' onClick={toggleTheme}>
+          تغییر تم
+        </button>
+      </div>
+    )
   }
 
   return content
